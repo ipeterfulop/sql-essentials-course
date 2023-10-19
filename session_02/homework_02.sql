44c004d42138040a2d599ab51fd4d44f7fae9fc0
Peter Fulop <contact@peterfulop.me>
2023-10-19 16:03:27 +0200

    added homework related to the 2nd learning session

---
+++ session_02/homework_02.sql
@@ -0,0 +1,54 @@
+-- HW 2.1
+-- The company has mandated that every employee in the roles of Data Scientist or Data Engineer must be under the
+-- supervision of a team lead. Please provide a query that presents the id, first name, last name, and position of
+-- employees who do not currently have a designated team lead (lead_employee_id is NULL), sorted in alphabetical order
+-- by their last name, followed by their first name.
+
+-- your solution for the HW 2.1 goes here
+
+
+-- HW 2.2
+-- The company has revised its policy regarding mandatory medical examinations. As per the updated policy,
+-- all employees are now required to undergo a medical analysis every 18 months. Employees who have not had a
+-- medical examination since their date of hire must schedule a medical examination within the next 6 weeks.
+-- Please provide a query that generates a list of all employees, including their first name, last name, date of
+-- their last medical analysis, and the date when their next analysis is scheduled.
+
+-- your solution for the HW 2.2 goes here
+
+-- HW 2.3
+-- Compose a query that produces the first name, last name, hire date, and date of the most recent medical analysis
+-- for employees who have a non-empty (non-NULL) date of their last medical analysis, and where this date is earlier
+-- than their hire date.
+
+-- your solution for the HW 2.3 goes here
+
+-- HW 2.4
+-- Display the three highest distinct salaries among Junior employees. An employee is categorized as Junior
+-- if they belong to either Junior 1 or Junior 2 band.
+
+-- your solution for the HW 2.4 goes here
+
+-- HW 2.5
+-- Retrieve a list of Data Scientists who are female and are currently working in Hungary.
+
+-- your solution for the HW 2.5 goes here
+
+-- HW 2.6
+-- Compose a query that fetches column details for the "countries" table, including column name, data type, whether
+-- it allows null values, and the maximum size where applicable.
+
+-- your solution for the HW 2.6 goes here
+
+-- H 2.7
+-- Provide a list of employees' full names and personal identification numbers, sorted in ascending order based on
+-- the last two characters of their personal identification numbers.
+
+-- your solution for the HW 2.7 goes here
+
+-- HW 2.8
+-- Generate a list of employees' full names, their salaries, and their yearly leave entitlements for those whose salary
+-- and yearly leave days have the same leftmost digit.
+
+-- your solution for the HW 2.8 goes here
+
